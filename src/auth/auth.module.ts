2f2bc3f2fc4121a730f86801abdf2481c3bbb0ea
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-12-09 17:03:45 -0300

    Estuturando o token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { UsuarioModule } from '../usuario/usuario.module';
+import { jwtConstants } from './constants/constant';
+import { AuthController } from './controllers/auth.controller';
 import { Bcrypt } from './bcrypt/bcrypt';
+import { AuthService } from './services/auth.service';
+import { LocalStrategy } from './strategy/local.strategy';
 
 @Module({
-  imports: [],
-  providers: [Bcrypt],
-  controllers: [],
+  imports: [
+    forwardRef(() => UsuarioModule),
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [Bcrypt, AuthService, LocalStrategy],
   exports: [Bcrypt],
 })
 export class AuthModule {}
