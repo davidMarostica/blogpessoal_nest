15272eb18a8a75f4c30bf049928cea2e482339ca
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-12-21 11:05:40 -0300

    Deploy do Projeto Blog Pessoal

---
+++ src/data/services/dev.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Postagem } from '../../postagem/entities/postagem.entity';
+import { Usuario } from '../../usuario/entities/usuario.entity';
+import { Tema } from '../../Tema/entities/tema.entity';
+
+@Injectable()
+export class DevService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'generetion',
+      password: '123',
+      database: 'db_blogpessoal',
+      entities: [Postagem, Tema, Usuario],
+      synchronize: true,
+    };
+  }
+}
